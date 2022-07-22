55f7937500df364db9e1bf1a3c427fe3dd73ef66
f5rcbh <f6807@hotmail.com>
2022-07-22 14:34:00 +0300

    finish 90% of desktop version

---
+++ src/components/img.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Perfume from '../images/image-product-desktop.jpg'
+
+function img() {
+  return (
+    <div className='img'>
+        <img src={Perfume} alt="product"/>
+    </div>
+  )
+}
+
+export default img
